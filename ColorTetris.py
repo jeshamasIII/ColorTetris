9d1d025b08ca3680fb339c48cc7d1af8d641efa5
Jimmy Shamas <jeshamasIII@gmail.com>
2020-09-11 22:39:11 -0600

    Tried to remove magic numbers. Some changes to function, attribute, and variables names.

--- ColorTetris.py
+++ ColorTetris.py
@@ -4,57 +4,63 @@ import curses
 import random
 
 
-class Piece:
-    def __init__(self, indices, center, color):
+class TetrisPiece:
+    def __init__(self, indices, center_of_rotation, color):
         self.indices = indices
-        self.center = center
+        self.center_of_rotation = center_of_rotation
         self.last_move_overlap = False
         self.color = color
 
 
-class Board:
-    def __init__(self, array_length, array_width):
-        self.array_length = array_length
-        self.array_width = array_width
-        self.array = [[0] * self.array_width for _ in range(self.array_length)]
+class TetrisBoard:
+    def __init__(self, num_rows, num_columns):
+        self.num_rows = num_rows
+        self.num_columns = num_columns
+
+        self.array = [[0] * self.num_columns for _ in range(self.num_rows)]
         self.active_piece = None
 
-    def in_bounds(self, temp_indices):
-        return all(0 <= i < self.array_length and 0 <= j < self.array_width
-                   for i, j in temp_indices)
+    def in_bounds(self, temp_active_piece_indices):
+        return all(0 <= i < self.num_rows and 0 <= j < self.num_columns
+                   for i, j in temp_active_piece_indices)
 
-    def no_overlap(self, temp_indices):
+    def no_overlap(self, temp_active_piece_indices):
         return all(self.array[i][j] == 0 for i, j in
-                   set(temp_indices) - set(self.active_piece.indices))
+                   set(temp_active_piece_indices) - set(self.active_piece.indices))
 
     def add_piece(self, piece):
-        # try to place Piece near top center of screen
-        temp_indices = [(i, j + int(self.array_width / 2) - 1)
-                        for i, j in piece.indices]
+        # try to place Piece near top center of board
+        new_active_piece_indices = [(i, j + int(self.num_columns / 2) - 1)
+                                    for i, j in piece.indices]
 
-        if all(self.array[i][j] == 0 for i, j in temp_indices):
+        if all(self.array[i][j] == 0 for i, j in new_active_piece_indices):
             self.active_piece = piece
-            self.update_array(temp_indices)
-            piece.indices = temp_indices
-            piece.center[1] += self.array_width // 2 - 1
+            self.update_array(new_active_piece_indices)
+            piece.indices = new_active_piece_indices
+            piece.center_of_rotation[1] += self.num_columns // 2 - 1
             piece.last_move_overlap = False
         else:
             piece.last_move_overlap = True
 
-    def rotate(self, piece):
-        # rotates Piece indices 90 degrees counter clockwise using a rotation
-        # matrix
-        x, y = piece.center
+    def rotate_active_piece(self):
+        # rotates active piece indices 90 degrees counter clockwise about it's
+        # center of a rotation
 
-        temp_indices = [(int(-j + y + x), int(i - x + y))
-                        for i, j in piece.indices]
+        x, y = self.active_piece.center_of_rotation
 
-        if (self.in_bounds(temp_indices)
-                and self.no_overlap(temp_indices)):
-            self.update_array(temp_indices)
-            piece.indices = temp_indices
+        # this translates the active piece so that it's center is now
+        # the origin, then rotates each point in indices about the origin,
+        # then translates the piece so that it's center is at it's
+        # original position
+        temp_active_piece_indices = [(int(-j + y + x), int(i - x + y))
+                                     for i, j in self.active_piece.indices]
 
-    def translate(self, piece, direction):
+        if (self.in_bounds(temp_active_piece_indices)
+                and self.no_overlap(temp_active_piece_indices)):
+            self.update_array(temp_active_piece_indices)
+            self.active_piece.indices = temp_active_piece_indices
+
+    def translate_active_piece(self, direction):
         if direction == 'right':
             x, y = 0, 1
         elif direction == 'left':
@@ -62,23 +68,25 @@ class Board:
         elif direction == 'down':
             x, y = 1, 0
 
-        temp_indices = [(i + x, j + y) for i, j in piece.indices]
-        if (self.in_bounds(temp_indices)
-                and self.no_overlap(temp_indices)):
-            self.update_array(temp_indices)
+        temp_active_piece_indices = [(i + x, j + y)
+                                     for i, j in self.active_piece.indices]
+        if (self.in_bounds(temp_active_piece_indices)
+                and self.no_overlap(temp_active_piece_indices)):
+            self.update_array(temp_active_piece_indices)
+            self.active_piece.indices = temp_active_piece_indices
+            self.active_piece.center_of_rotation[0] += x
+            self.active_piece.center_of_rotation[1] += y
 
-            piece.indices = temp_indices
-            piece.center[0] += x
-            piece.center[1] += y
+            self.active_piece.last_move_overlap = False
 
-            piece.last_move_overlap = False
+        elif (self.in_bounds(temp_active_piece_indices)
+              and not self.no_overlap(temp_active_piece_indices)):
+            self.active_piece.last_move_overlap = True
 
-        elif (self.in_bounds(temp_indices)
-              and not self.no_overlap(temp_indices)):
-            piece.last_move_overlap = True
-
-        elif not self.in_bounds(temp_indices) and direction == 'down':
-            piece.last_move_overlap = True
+        # this is necessary to tell when a piece hits the the bottom of the
+        # board
+        elif not self.in_bounds(temp_active_piece_indices) and direction == 'down':
+            self.active_piece.last_move_overlap = True
 
     def update_array(self, new_indices):
         for i, j in self.active_piece.indices:
@@ -102,34 +110,26 @@ class CursesWindow:
         self.window.addstr(y, x, string)
 
 
-class ScoreWindow(CursesWindow):
-    def __init__(self, game, window_length, window_width):
-        CursesWindow.__init__(self, game)
-        self.window = curses.newwin(window_length, window_width,
-                                    0, game.board.array_width + 3)
-        self.window.border('*', '*', '*', '*', '*', '*', '*', '*')
-        self.update()
-
-    def update(self):
-        self.window.addstr(1, 1, f'Score:{self.game.score}')
-        self.window.addstr(2, 1, f'Lines:{self.game.lines_completed}')
-        self.window.addstr(3, 1, f'Level:{self.game.level}')
-        self.window.refresh()
-
-
 class BoardWindow(CursesWindow):
     def __init__(self, game):
         CursesWindow.__init__(self, game)
+
+        # the window's border adds two extra rows and two extra columns
+        self.num_rows = game.board.num_rows + 2
+        self.num_columns = game.board.num_columns + 2
+
         self.window = curses.newwin(
-            game.board.array_length + 2,
-            game.board.array_width + 2
+            self.num_rows,
+            self.num_columns
         )
+
+        self.window.border('*', '*', '*', '*', '*', '*', '*', '*')
         self.update()
 
     def update(self):
-        self.window.border('*', '*', '*', '*', '*', '*', '*', '*')
-        for i in range(self.game.board.array_length):
-            for j in range(self.game.board.array_width):
+        # only update the interior of the window
+        for i in range(self.num_rows - 2):
+            for j in range(self.num_columns - 2):
                 if self.game.board.array[i][j] != 0:
                     self.window.addstr(
                         i + 1,
@@ -151,18 +151,58 @@ class BoardWindow(CursesWindow):
         return self.window.getch()
 
 
-class PreviewWindow(CursesWindow):
-    def __init__(self, game, window_length, window_width):
+class ScoreWindow(CursesWindow):
+    def __init__(self, game, board_window):
         CursesWindow.__init__(self, game)
-        self.window = curses.newwin(window_length, window_width,
-                                    5, game.board.array_width + 3)
+        self.num_items_to_display = 3
+
+        # the window's border adds two extra rows
+        self.num_rows = self.num_items_to_display + 2
+
+        # this is somewhat arbitrary
+        self.num_columns = 14
+
+        self.window = curses.newwin(
+            self.num_rows,
+            self.num_columns,
+            0,
+            board_window.num_columns + 1
+        )
+
+        self.window.border('*', '*', '*', '*', '*', '*', '*', '*')
+        self.update()
+
+    def update(self):
+        self.window.addstr(1, 1, f'Score:{self.game.score}')
+        self.window.addstr(2, 1, f'Lines:{self.game.lines_completed}')
+        self.window.addstr(3, 1, f'Level:{self.game.level}')
+        self.window.refresh()
+
+
+class PiecePreviewWindow(CursesWindow):
+    def __init__(self, game, board_window, score_window):
+        CursesWindow.__init__(self, game)
+
+        # the window's border adds two extra rows and two extra columns
+        self.num_rows = game.max_piece_length + 2
+        self.num_columns = game.max_piece_length + 2
+
+        self.window = curses.newwin(
+            self.num_rows,
+            self.num_columns,
+            score_window.num_rows,
+            board_window.num_columns + 1
+        )
+
         self.update()
 
     def update(self):
         self.window.erase()
         self.window.border('*', '*', '*', '*', '*', '*', '*', '*')
-        for i in range(4):
-            for j in range(4):
+
+        # only update the interior of the window
+        for i in range(self.num_rows - 2):
+            for j in range(self.num_columns - 2):
                 if (i, j) in self.game.next_piece.indices:
                     self.window.addstr(
                         i + 1,
@@ -190,8 +230,9 @@ class GUI:
         curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
         self.board_window = BoardWindow(game)
-        self.score_window = ScoreWindow(game, 5, 14)
-        self.piece_preview_window = PreviewWindow(game, 6, 6)
+        self.score_window = ScoreWindow(game, self.board_window)
+        self.piece_preview_window = PiecePreviewWindow(game, self.board_window,
+                                                       self.score_window)
 
         curses.noecho()
         curses.cbreak()
@@ -201,25 +242,29 @@ class GUI:
 
 
 class Game:
-    SPACE_KEY = 32
-
-    def __init__(self, board_length, board_width):
-        self.board = Board(board_length, board_width)
+    def __init__(self, board_num_rows, board_num_columns):
+        self.board = TetrisBoard(board_num_rows, board_num_columns)
 
         self.score = 0
         self.lines_completed = 0
         self.level = 0
+
+        self.SPACE_KEY_ASCII_VALUE = 32
+
+        # approximate frame rate
         self.frame_rate = 60
 
         self.pieces = [
-            Piece([(0, 1), (1, 1), (2, 1), (3, 1)], [1.5, 1.5], 1),  # I
-            Piece([(0, 1), (1, 1), (2, 1), (2, 2)], [1, 1], 2),  # J
-            Piece([(0, 1), (1, 1), (2, 1), (2, 0)], [1, 1], 3),  # L
-            Piece([(0, 0), (0, 1), (1, 0), (1, 1)], [.5, .5], 4),  # O
-            Piece([(1, 0), (1, 1), (0, 1), (0, 2)], [1, 1], 5),  # S
-            Piece([(1, 0), (1, 1), (1, 2), (0, 1)], [1, 1], 6),  # T
-            Piece([(0, 0), (0, 1), (1, 1), (1, 2)], [1, 1], 7)  # Z
+            TetrisPiece([(0, 1), (1, 1), (2, 1), (3, 1)], [1.5, 1.5], 1),  # I
+            TetrisPiece([(0, 1), (1, 1), (2, 1), (2, 2)], [1, 1], 2),  # J
+            TetrisPiece([(0, 1), (1, 1), (2, 1), (2, 0)], [1, 1], 3),  # L
+            TetrisPiece([(0, 0), (0, 1), (1, 0), (1, 1)], [.5, .5], 4),  # O
+            TetrisPiece([(1, 0), (1, 1), (0, 1), (0, 2)], [1, 1], 5),  # S
+            TetrisPiece([(1, 0), (1, 1), (1, 2), (0, 1)], [1, 1], 6),  # T
+            TetrisPiece([(0, 0), (0, 1), (1, 1), (1, 2)], [1, 1], 7)  # Z
         ]
+
+        self.max_piece_length = 4
 
         self.next_piece = deepcopy(random.choice(self.pieces))
 
@@ -239,38 +284,43 @@ class Game:
             keyboard_input = self.GUI.board_window.getch()
 
             loop_count += 1
+
             force_move = (loop_count % max(self.frame_rate - self.level, 1) == 0)
-            hard_drop = (keyboard_input == self.SPACE_KEY)
+            hard_drop = (keyboard_input == self.SPACE_KEY_ASCII_VALUE)
             if force_move or hard_drop:
                 if hard_drop:
                     while not self.board.active_piece.last_move_overlap:
-                        self.board.translate(self.board.active_piece, 'down')
+                        self.board.translate_active_piece('down')
 
                     self.GUI.board_window.update()
                     time.sleep(.5)
 
                 elif force_move:
-                    self.board.translate(self.board.active_piece, 'down')
+                    self.board.translate_active_piece('down')
 
                 if self.board.active_piece.last_move_overlap:
-                    # clear lines one at a time starting from top of screen
+                    # try to clear lines one at a time starting from the top of
+                    # the screen
                     line_count = 0
                     for row_number, row in enumerate(self.board.array):
                         if all(row):
-                            line_count += 1
-
-                            del self.board.array[row_number]
-                            self.board.array.insert(0, [0] * self.board.array_width)
-
+                            # highlight row to be deleted
+                            # add 1 to row_number because of board_window's border
                             self.GUI.board_window.addstr(
-                                row_number + 1, 1, '=' * self.board.array_width
+                                row_number + 1, 1, '=' * self.board.num_columns
                             )
 
                             self.GUI.board_window.refresh()
                             time.sleep(.5)
 
+                            # delete row
+                            del self.board.array[row_number]
+                            self.board.array.insert(0, [0] * self.board.num_columns)
+
                             self.GUI.board_window.update()
                             time.sleep(.5)
+
+                            line_count += 1
 
                     self.score += self.points(line_count)
                     self.lines_completed += line_count
@@ -290,18 +340,19 @@ class Game:
 
             else:
                 if keyboard_input == ord('w'):
-                    self.board.rotate(self.board.active_piece)
+                    self.board.rotate_active_piece()
                 if keyboard_input == ord('d'):
-                    self.board.translate(self.board.active_piece, 'right')
+                    self.board.translate_active_piece('right')
                 if keyboard_input == ord('s'):
-                    self.board.translate(self.board.active_piece, 'down')
+                    self.board.translate_active_piece('down')
                 if keyboard_input == ord('a'):
-                    self.board.translate(self.board.active_piece, 'left')
+                    self.board.translate_active_piece('left')
                 # exit game
                 if keyboard_input == ord('e'):
                     break
 
             self.GUI.board_window.update()
+
             # delay after a rotation
             if keyboard_input == ord('w'):
                 time.sleep(.25)
@@ -321,5 +372,5 @@ class Game:
 
 
 # Run the game
-game = Game(board_length=16, board_width=10)
+game = Game(board_num_rows=16, board_num_columns=10)
 game.main_loop()
